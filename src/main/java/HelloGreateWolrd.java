5a413600583a1fd4cc88a1b1f606ad27d66072b3
kypej <37840507+kypej@users.noreply.github.com>
2020-07-28 18:47:28 +0300

    Update HelloGreateWolrd.java

--- src/main/java/HelloGreateWolrd.java
+++ src/main/java/HelloGreateWolrd.java
@@ -1,5 +1,5 @@
 public class HelloGreateWolrd {
     public static void main(String[] args) {
-        System.out.println("Ольга, я тебя люблю!");
+        System.out.println("Mir trud mai");
     }
 }
